1595736ed63502daa3644a748e91ab9a1662d830
Travis Pronschinske <>
2014-03-22 18:51:03 -0500

    Version 15- POS Completed - With Payment Strategy for CC

---
+++ src/Lab01/Invoice.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Lab01;
+
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+/**
+ *
+ * @author tpronschinske
+ */
+public class Invoice {
+
+    private Calendar invoiceDate;
+    private int gracePeriod = 15;
+    private Calendar dueDate;
+    private Date date;
+    private SimpleDateFormat simpledateformat;
+    
+    public Invoice() {
+        date = new Date();
+        invoiceDate = Calendar.getInstance(); 
+    }
+
+
+    public String getFormattedDate(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yy HH:mm ");
+        return dateFormat.format(getInvoiceDate().getTime());
+    }
+    
+    
+    
+    public Calendar getChangedDueDate(){
+        dueDate = invoiceDate;
+        dueDate.add(Calendar.DATE, gracePeriod);
+        return dueDate;
+    }
+    
+    
+    public String getDueDateToString(){
+        simpledateformat = new SimpleDateFormat("MM/dd/yy");
+        dueDate = invoiceDate;
+        dueDate.add(Calendar.DATE, gracePeriod);
+        date.getTime();
+        return simpledateformat.format(getDate());
+    }
+    
+    
+    public final Calendar getInvoiceDate() { 
+        return invoiceDate; 
+    }
+
+    public final void setInvoiceDate(Calendar invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public int getGracePeriod() {
+        return gracePeriod;
+    }
+
+    public void setGracePeriod(int gracePeriod) {
+        this.gracePeriod = gracePeriod;
+    }
+
+    public Calendar getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Calendar dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public SimpleDateFormat getSimpledateformat() {
+        return simpledateformat;
+    }
+
+    public void setSimpledateformat(SimpleDateFormat simpledateformat) {
+        this.simpledateformat = simpledateformat;
+    }
+    
+    
+    
+}
---
+++ src/Lab01/StartUp.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Lab01;
+
+
+
+public class StartUp {
+
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+//        Date date = new Date();
+//        Calendar c = Calendar.getInstance();
+//        date = c.getTime();
+//        System.out.println(date);
+        
+       Invoice i = new Invoice();
+       
+        System.out.println(i.getInvoiceDate().getTime());
+        System.out.println(i.getChangedDueDate().getTime());
+        System.out.println(i.getDueDateToString());
+
+
+        
+        
+    }
+    
+}
