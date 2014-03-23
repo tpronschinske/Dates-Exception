40d1bd04c53c3f008e590bebb8ab9a04a34749e4
Travis Pronschinske <>
2014-03-22 19:18:11 -0500

    Lab 01 & 02- Completed

---
+++ src/Lab02/DateService.java
@@ -0,0 +1,47 @@
+/*
+ * this class should have the ability to convert a date string to calendar and date
+ *
+ */
+
+package Lab02;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.ParseException;
+
+/**
+ *
+ * @author Travis
+ */
+public class DateService {
+    
+    
+    public final void convertStringToCalendar(String formattedDateString){
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy HH:mm a");
+        try{
+            Date date = calendar.getTime();
+            calendar.setTime(dateFormat.parse(formattedDateString));
+            System.out.println(dateFormat.format(date));
+        } catch (ParseException parseException){
+            System.out.println("Format Not Accepted: Must Be In This Format (Month/day/year time (am or pm) EX: 12/10/1987 12:00 PM");
+            
+        }
+        
+        
+    }
+    
+    public final void convertStringToDate(String formattedDateString){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            Date date = dateFormat.parse(formattedDateString);
+            System.out.println(dateFormat.format(date));
+        } catch (ParseException parseException) {
+            System.out.println("Format Not Accepted: Must Be Month/Day/Year (12/10/1987)");
+        }
+                
+        
+    }
+    
+}
--- src/Lab02/StartUp.java
+++ src/Lab02/StartUp.java
@@ -11,5 +11,24 @@ package Lab02;
  * @author tpronschinske
  */
 public class StartUp {
+   
+    public static void main(String[] args) {
+        
+        
+        String date;
+        
+        DateService dateService = new DateService();
+        date = "12/22/2014";
+        dateService.convertStringToDate(date);
+        
+        
+        // having issues with the convertStringToCalendar
+        date = "December 10, 2014 12:00 AM";
+        dateService.convertStringToCalendar(date);
     
+ 
+        
+        
+    }
+ 
 }
